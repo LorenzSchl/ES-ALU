35c7a71fdac45d3da223b5c9a0e2fec00e4f62be
CloudyDatFun <lorenz.macke@gmx.de>
2020-06-05 17:24:17 +0000

    commands are done

--- ALUV1S.vhd
+++ ALUV1S.vhd
@@ -42,59 +42,51 @@ entity ALUV1S is
 end ALUV1S;
 
 architecture Behavioral of ALUV1S is
+signal temp:  STD_LOGIC_VECTOR(15 downto 0);
 begin
-	process(A, B, Cmd, CLK)	begin	
+	process(A, B, Cmd, CLK)	begin			
 		if rising_edge(CLK) then
 			case(Cmd) is
 				when "0000" => -- A+B
-					FLow <= ;
-					FHigh <= ;
+					temp <= A+B;
+					FLow <= temp(7 downto 0);
+					Cout <= temp(8);
 				when "0001" => -- A-B
-					FLow <= ;
-					FHigh <= ;
+					FLow <= A-B;
 				when "0010" => -- B-A
-					FLow <= ;
-					FHigh <= ;
+					FLow <= B-A;
 				when "0011" => -- A
-					FLow <= ;
-					FHigh <= ;
+					FLow <= A;
 				when "0100" => -- B
-					FLow <= ;
-					FHigh <= ;
+					FLow <= B;
 				when "0101" => -- -A
-					FLow <= ;
-					FHigh <= ;
+					FLow <= not A;
 				when "0110" => -- -B
-					FLow <= ;
-					FHigh <= ;
+					FLow <= not B;
 				when "0111" => -- sll(A)
-					FLow <= ;
-					FHigh <= ;
+					Cout <= A(7);
+					FLow <= std_logic_vector(unsigned(A) sll 1); -- Funktioniert nur im Simulator
 				when "1000" => -- slr(A)
-					FLow <= ;
-					FHigh <= ;
+					Cout <= A(0);
+					FLow <= std_logic_vector(unsigned(A) srl 1); -- Funktioniert nur im Simulator
 				when "1001" => -- rll(A)
-					FLow <= ;
-					FHigh <= ;
+					FLow <= std_logic_vector(unsigned(A) rol 1); -- Funktioniert nur im Simulator
 				when "1010" => -- rlr(A)
-					FLow <= ;
-					FHigh <= ;
+					FLow <= std_logic_vector(unsigned(A) ror 1); -- Funktioniert nur im Simulator
 				when "1011" => -- A*B
-					FLow <= ;
-					FHigh <= ;
+					temp <= std_logic_vector(to_unsigned((to_integer(unsigned(A)) * to_integer(unsigned(B))), 16));
+					FLow <= temp(7 downto 0);
+					FHigh <= temp(15 downto 8);
 				when "1100" => -- NAND(A,B)
-					FLow <= ;
-					FHigh <= ;
+					FLow <= A nand B;
 				when "1101" => -- XOR(A,B)
-					FLow <= ;
-					FHigh <= ;
+					FLow <= A xor B;
 				when "1110" => -- CRC_MEM
-					FLow <= ;
-					FHigh <= ;
+					FLow <= 0;
+					FHigh <= 0;
 				when "1111" => -- RESERVED
-					FLow <= ;
-					FHigh <= ;
-		end if
+			end case;
+		end if;
 	end process;
 end Behavioral;
 
