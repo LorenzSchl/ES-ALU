5c769747e224301ffceff6c41cd2ec903ca90f04
LorenzMacke <lorenz.macke@edu.fhdw.de>
2020-06-14 19:02:40 +0200

    Fix to multiplication

--- VHDL_Generator.py
+++ VHDL_Generator.py
@@ -4,7 +4,7 @@ EXPECTED_FILE_PATH = "S:/Uni/expected.txt"
 OPERANTS = [
     (lambda a, b : (bin(a+b & 0b11111111), bin(a+b > 255)), "0000"),
     (lambda a, b : (bin(a-b & 0b11111111) if a>b else bin(0b00000000), bin(a<b)), "0001"),
-    (lambda a, b : (bin(b-a & 0b11111111) if a<b else bin(0b00000000), bin(b<a)), "0010"),
+    (lambda a, b : (bin(b-a & 0b11111111) if b>a else bin(0b00000000), bin(b<a)), "0010"),
     (lambda a, b : (bin(a), bin(0)), "0011"),
     (lambda a, b : (bin(b), bin(0)), "0100"),
     (lambda a, b : (bin(flip_bits(bin(a))), bin(0)), "0101"),
@@ -25,7 +25,7 @@ def main():
                 if(op == "1011"):
                     (flow, carry) = func(a, b)
                     flow = flow[2:].zfill(16)
-                    write_expected(f"{flow[-8:]} {flow[:8]} {carry[2:]} {bin(a==b)[2:]}")
+                    write_expected(f"{flow[:8]} {flow[-8:]} {carry[2:]} {bin(a==b)[2:]}")
                     write_stimuli(f"{bin(a)[2:].zfill(8)} {bin(b)[2:].zfill(8)} {op}")
                 else:
                     (flow, carry) = func(a, b)
